f136c2c41677d151414278bf0df3d168b36587f6
unknown <alvinjoyner3@gmail.com>
2023-08-22 15:46:11 -0400

    testing authenicaion errors

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,41 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
+// Exported module
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // Custom authentication middleware function
+  customAuthMiddleware: function ({ req }) {
+    let authToken = req.headers['x-custom-auth-token'];
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    if (!authToken) {
+      return req;
     }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      const decodedToken = customVerifyToken(authToken); // Custom token verification function
+      req.userInfo = decodedToken.data;
+    } catch (error) {
+      console.error('Invalid custom token:', error.message);
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
+
+  // Function for signing a token
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+// Custom token verification function
+function customVerifyToken(token) {
+  // Implement your custom token verification logic here
+  // Return an object with { isValid, data } where "isValid" indicates token validity
+  // and "data" contains user information from the token
+  // Example: return { isValid: true, data: { userId: '123', role: 'user' } }
+}
